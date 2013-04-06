5af7ae3659d546168cd1eb37f46c84bb549340b9
Devon Campbell <raddevon@gmail.com>
2013-04-06 19:12:03 -0400

    Merged #1 from bstrdsmkr/master with fixes

--- pyspeak.py
+++ pyspeak.py
@@ -1,39 +1,40 @@
 # Gets data from the ThingSpeak API
 
-import urllib
-import urllib2
+import urllib.request, urllib.parse
+
 try:
     import json
 except:
     import simplejson as json
 
 
-class ThingSpeak(self):
+class ThingSpeak:
     def __init__(self, read_key='', write_key=''):
         self.API = 'http://api.thingspeak.com/'
         self.ready_key = read_key
-        seld.write_key = write_key
+        self.write_key = write_key
     
     def get_channel(self, channel_id, last_entry=False, fmt='json', opts={}):
-        '''
+        """
         Parameters:
         channel_id (int or str) - The id of the channel to be retrieved
         last_entry (bool) - If True, only the most recent value will be returned
         fmt (str) - json, csv, or xml. Returns the data as on object of the
             given type
         opts (dict) - A dict of optional values to be passed via url params
-        '''
-        if last entry: endpoint = 'last.{}'.format(fmt)
+        """
+        if last_entry: endpoint = 'last.{}'.format(fmt)
         else: endpoint = 'feed.{}'.format(fmt)
         
         url = '{}channels/{}/{}'.format(self.API, channel_id, endpoint)
         if opts:
-            queries = urllib.urlencode(opts)
+            queries = urllib.parse.urlencode(opts)
             url = '{}?{}'.format(url, queries)
-        
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        result = response.read()
+
+        req = urllib.request.Request(url)
+        response = urllib.request.urlopen(req)
+        encoding = response.headers.get_content_charset()
+        result = response.read().decode(encoding)
         
         if fmt == 'json':
             result = json.loads(result)
@@ -41,7 +42,7 @@ class ThingSpeak(self):
         return result
     
     def get_field(self, channel_id, field_id, last_entry=False, fmt='json', opts={}):
-        '''
+        """
         Parameters:
         channel_id (int or str) - The id of the channel to be retrieved
         field_id (int or str) - The id of the field to be retrieved
@@ -49,18 +50,19 @@ class ThingSpeak(self):
         fmt (str) - json, csv, or xml. Returns the data as on object of the
             given type
         opts (dict) - A dict of optional values to be passed via url params
-        '''
-        if last entry: endpoint = '{}/last.{}'.format(field_id, fmt)
+        """
+        if last_entry: endpoint = '{}/last.{}'.format(field_id, fmt)
         else: endpoint = '{}.{}'.format(field_id, fmt)
         
         url = '{}channels/{}/field/{}'.format(self.API, channel_id, endpoint)
         if opts:
-            queries = urllib.urlencode(opts)
+            queries = urllib.parse.urlencode(opts)
             url = '{}?{}'.format(url, queries)
         
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        result = response.read()
+        req = urllib.request.Request(url)
+        response = urllib.request.urlopen(req)
+        encoding = response.headers.get_content_charset()
+        result = response.read().decode(encoding)
         
         if fmt == 'json':
             result = json.loads(result)
@@ -68,19 +70,19 @@ class ThingSpeak(self):
         return result
 
     def update_channel(self, opts):
-        '''
+        """
         Parameters:
         opts (dict) - A dict of values to be passed vie url params
-        '''
+        """
         if not 'key' in opts:
             if not self.write_key:
                 raise ValueError('A write key is required to update a channel')
             else: opts['key'] = self.write_key
 
-        opts = urllib.encode(ops)        
+        opts = urllib.parse.urlencode(ops)
         url = '{}/update?{}'.format(self.API, opts)
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
+        req = urllib.request.Request(url)
+        response = urllib.request.urlopen(req)
         result = response.read()
 
         return result
