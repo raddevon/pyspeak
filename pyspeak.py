4d2fe8b03ac0bbe3bc498892669cbd786360bd96
Zach Guzman <bstrdsmkr@gmail.com>
2013-04-06 15:46:40 -0400

    Rewrote methods as a class
    - Fix passing of optional parameters
    - Added update_channel() method
    - Added ability to return different data formats

--- pyspeak.py
+++ pyspeak.py
@@ -1,41 +1,86 @@
 # Gets data from the ThingSpeak API
 
-import urllib.request, simplejson as json
-from urllib.error import HTTPError
+import urllib
+import urllib2
+try:
+    import json
+except:
+    import simplejson as json
 
-API_ADDRESS = 'http://api.thingspeak.com/' # The URL of your ThingSpeak API.
 
-def read(url, **kwargs):
-    '''
-    Takes a URL and returns a JSON object with the response
-    '''
-    try:
-        response =  urllib.request.urlopen(url, kwargs)
-    except ValueError or HTTPError:
-        response =  urllib.request.urlopen(url)
-    return json.load(response)
+class ThingSpeak(self):
+    def __init__(self, read_key='', write_key=''):
+        self.API = 'http://api.thingspeak.com/'
+        self.ready_key = read_key
+        seld.write_key = write_key
+    
+    def get_channel(self, channel_id, last_entry=False, fmt='json', opts={}):
+        '''
+        Parameters:
+        channel_id (int or str) - The id of the channel to be retrieved
+        last_entry (bool) - If True, only the most recent value will be returned
+        fmt (str) - json, csv, or xml. Returns the data as on object of the
+            given type
+        opts (dict) - A dict of optional values to be passed via url params
+        '''
+        if last entry: endpoint = 'last.{}'.format(fmt)
+        else: endpoint = 'feed.{}'.format(fmt)
+        
+        url = '{}channels/{}/{}'.format(self.API, channel_id, endpoint)
+        if opts:
+            queries = urllib.urlencode(opts)
+            url = '{}?{}'.format(url, queries)
+        
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        result = response.read()
+        
+        if fmt == 'json':
+            result = json.loads(result)
+        
+        return result
+    
+    def get_field(self, channel_id, field_id, last_entry=False, fmt='json', opts={}):
+        '''
+        Parameters:
+        channel_id (int or str) - The id of the channel to be retrieved
+        field_id (int or str) - The id of the field to be retrieved
+        last_entry (bool) - If True, only the most recent value will be returned
+        fmt (str) - json, csv, or xml. Returns the data as on object of the
+            given type
+        opts (dict) - A dict of optional values to be passed via url params
+        '''
+        if last entry: endpoint = '{}/last.{}'.format(field_id, fmt)
+        else: endpoint = '{}.{}'.format(field_id, fmt)
+        
+        url = '{}channels/{}/field/{}'.format(self.API, channel_id, endpoint)
+        if opts:
+            queries = urllib.urlencode(opts)
+            url = '{}?{}'.format(url, queries)
+        
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        result = response.read()
+        
+        if fmt == 'json':
+            result = json.loads(result)
+        
+        return result
 
-def read_channel(channel_id, last_entry=False, **kwargs):
-    '''
-    Parameters:
-    channel_id- the id of a ThingSpeak channel
-    last_entry- Pass True to get only the most recent entry
+    def update_channel(self, opts):
+        '''
+        Parameters:
+        opts (dict) - A dict of values to be passed vie url params
+        '''
+        if not 'key' in opts:
+            if not self.write_key:
+                raise ValueError('A write key is required to update a channel')
+            else: opts['key'] = self.write_key
 
-    Returns the JSON response
-    '''
-    url = '{}channels/{}/feed.json'.format(API_ADDRESS, channel_id) if not last_entry\
-            else '{}channels/{}/last.json'.format(API_ADDRESS, channel_id)
-    return read(url, **kwargs)
+        opts = urllib.encode(ops)        
+        url = '{}/update?{}'.format(self.API, opts)
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        result = response.read()
 
-def read_field(channel_id, field_id, last_entry=False, **kwargs):
-    '''
-    Parameters:
-    channel_id- the id of a ThingSpeak channel
-    field_id- the id of a field in the given channel
-    last_entry- pass True to get only the most recent entry
-
-    Returns the JSON response
-    '''
-    url = '{}channels/{}/field/{}.json'.format(API_ADDRESS, channel_id, field_id) if not last_entry\
-    else '{}channels/{}/field/{}/last.json'.format(API_ADDRESS, channel_id, field_id)
-    return read(url, **kwargs)
+        return result
