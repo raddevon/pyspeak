c6f899809ee586a63658b7ca6bd734da7dfa858c
Devon Campbell <raddevon@gmail.com>
2013-05-06 09:56:37 -0400

    Prints in testing module for debugging

--- tests/config.sample.py
+++ tests/config.sample.py
@@ -3,7 +3,10 @@ If you want to run tests on pyspeak, you must create a channel and add that chan
 """
 API = 'http://api.thingspeak.com/'
 # Your channel's read and write keys as strings. From your channel's page, click the API Keys tab.
-WRITE_KEY =
-READ_KEY =
-CHANNEL = # Channel number as string or number
-FIELDS = ['field1', 'field2', 'field3', 'field4'] # An array of fields to test. Enter each field as a string.
+# WRITE_KEY =
+# READ_KEY =
+# Channel number as string or number
+# CHANNEL =
+# An array of fields to test. Enter each field as a string. Do not use field labels. Use the actual field names as in
+# the samples below.
+# FIELDS = ['field1', 'field2', 'field3', 'field4']
--- tests/test_pyspeak.py
+++ tests/test_pyspeak.py
@@ -1,22 +1,38 @@
 import pyspeak
+from tests.config import API, CHANNEL, READ_KEY, WRITE_KEY, FIELDS
+from random import random
 
 class TestPySpeak:
 
     def setup(self):
-        self.test_channel = pyspeak.Channel(9)
+        self.test_channel = pyspeak.Channel(CHANNEL, API, READ_KEY, WRITE_KEY)
+        self.updates = dict()
+
+    def generate_random_update_values(self):
+        for field in FIELDS:
+            self.updates[field] = random()
 
     def test_get_channel_feed_is_dictionary(self):
         test_channel_feed = self.test_channel.get_channel_feed()
         assert ( ( type(test_channel_feed) == dict ) & ( len(test_channel_feed) > 0 ) )
 
-    # def test_get_channel_feed_last_is_dictionary(self):
-    #     test_channel_feed = self.test_channel.get_channel_feed(last_entry=True)
-    #     assert ( ( type(test_channel_feed) == dict ) & ( len(test_channel_feed) == 1 ) )
-
     def test_get_field_feed_is_dictionary(self):
         test_field_feed = self.test_channel.get_field_feed('field2')
         assert ( ( type(test_field_feed) == dict ) & ( len(test_field_feed) > 0 ) )
 
-    # def test_get_field_feed_last_is_dictionary(self):
-    #     test_field_feed = self.test_channel.get_field_feed('field2', last_entry=True)
-    #     assert ( ( type(test_field_feed) == dict ) & ( len(test_field_feed) == 1 ) )
+    def test_update_and_read_channel(self):
+        self.generate_random_update_values()
+        self.test_channel.update_channel(self.updates)
+        test_json = self.test_channel.get_channel_feed(last_entry=True)
+        for field in FIELDS:
+            assert( float(test_json[field]) == self.updates[field] )
+
+    def test_update_and_read_field(self):
+        self.generate_random_update_values()
+        self.test_channel.update_channel(self.updates)
+        for field in FIELDS:
+            test_json = self.test_channel.get_field_feed(field, last_entry=True)
+            print(field)
+            print(test_json[field])
+            print(self.updates[field])
+            assert( float(test_json[field]) == self.updates[field] )
