e7af97371642b9d4b6e83fc26ca16e84d566bfe8
Devon Campbell <raddevon@gmail.com>
2013-06-10 21:33:39 -0400

    Tidied up and added DEST_URL

--- tests/config.sample.py
+++ tests/config.sample.py
@@ -1,12 +1,22 @@
 """
-If you want to run tests on pyspeak, you must create a channel and add that channel's information to this file.
+If you want to run tests on pyspeak, you must create a channel
+and add that channel's information to this file.
 """
 API = 'http://api.thingspeak.com/'
-# Your channel's read and write keys as strings. From your channel's page, click the API Keys tab.
+
+# Your channel's read and write keys as strings. From your channel's page,
+# click the API Keys tab.
 # WRITE_KEY =
 # READ_KEY =
+
 # Channel number as string or number
 # CHANNEL =
-# An array of fields to test. Enter each field as a string. Do not use field labels. Use the actual field names as in
-# the samples below.
+
+# An array of fields to test. Enter each field as a string.
+# Do not use field labels. Use the actual field names as in the samples below.
 # FIELDS = ['field1', 'field2', 'field3', 'field4']
+
+# A URL to test posting data fromt he channel.
+# Set to None if you do not want to test posting.
+# The URL must return the POST data in JSON format for testing to work.
+# DEST_URL = 'http://httpbin.org/post'
