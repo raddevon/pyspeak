b3ccdd24a7f33a09827aed2807d1ab4820cdb3c3
Devon Campbell <raddevon@gmail.com>
2013-10-02 10:05:44 -0400

    Moves tests folder inside package

---
+++ pyspeak/tests/config.py
@@ -0,0 +1,6 @@
+API = 'http://192.168.56.100:3000/'
+WRITE_KEY = 'CKU4AVPW64OXGB4H'
+READ_KEY = 'Q5JVYERMO755AZ8J'
+CHANNEL = 1
+FIELDS = ['field1', 'field2', 'field3', 'field4']
+DEST_URL = 'http://httpbin.org/post'
--- pyspeak/tests/test_channel.py
+++ pyspeak/tests/test_channel.py
@@ -1,12 +1,14 @@
-import pyspeak.pyspeak as pyspeak
-from tests.config import API, CHANNEL, READ_KEY, WRITE_KEY, FIELDS, DEST_URL
+from .. import Channel
+from .config import API, CHANNEL, READ_KEY, WRITE_KEY, FIELDS, DEST_URL
+import simplejson as json
 import random
 
 
 class TestPySpeak:
 
     def setup(self):
-        self.test_channel = pyspeak.Channel(CHANNEL, DEST_URL, API, READ_KEY, WRITE_KEY)
+        self.test_channel = Channel(
+            CHANNEL, DEST_URL, API, READ_KEY, WRITE_KEY)
         self.updates = dict()
 
     def generate_random_update_values(self):
@@ -15,11 +17,13 @@ class TestPySpeak:
 
     def test_get_channel_feed_is_dictionary(self):
         test_channel_feed = self.test_channel.get_channel_feed()
-        assert ( ( type(test_channel_feed) == dict ) & ( len(test_channel_feed) > 0 ) )
+        assert ((type(test_channel_feed) == dict)
+                & (len(test_channel_feed) > 0))
 
     def test_get_field_feed_is_dictionary(self):
         test_field_feed = self.test_channel.get_field_feed('field2')
-        assert ( ( type(test_field_feed) == dict ) & ( len(test_field_feed) > 0 ) )
+        assert ((type(test_field_feed) == dict)
+                & (len(test_field_feed) > 0))
 
     def test_update_and_read_channel(self):
         self.generate_random_update_values()
@@ -27,20 +31,19 @@ class TestPySpeak:
         test_json = self.test_channel.get_channel_feed(last_entry=True)
 
         for field in FIELDS:
-            assert( int(test_json[field]) == self.updates[field] )
+            assert(int(test_json[field]) == self.updates[field])
 
     def test_update_and_read_field(self):
         self.generate_random_update_values()
         self.test_channel.update_channel(self.updates)
         for field in FIELDS:
-            test_json = self.test_channel.get_field_feed(field, last_entry=True)
-            assert( int(test_json[field]) == self.updates[field] )
+            test_json = self.test_channel.get_field_feed(
+                field, last_entry=True)
+            assert(int(test_json[field]) == self.updates[field])
 
     def test_post_pyspeak_data(self):
         self.generate_random_update_values()
-        source_response = self.test_channel.get_channel_feed()
         self.test_channel.update_channel(self.updates)
+        source_response = json.dumps(self.test_channel.get_channel_feed())
         dest_response = self.test_channel.post_data()
-        print(dest_response.json()['form']['json_data'])
-        print(source_response)
-        assert( dest_response.json()['form']['json_data'] == source_response )
+        assert(dest_response.json()['form']['json_data'] == source_response)
